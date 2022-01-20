aeb3677dc16586ce8e491e555c2fe2ab52a4741b
fbu <fb@kreatize.com>
2022-01-20 10:25:38 +0100

    [express-app][package.json+app.js] added dotenv package and example file

--- express-app/app.js
+++ express-app/app.js
@@ -1,6 +1,7 @@
+require('dotenv').config({path: './.env.local'});
 const express = require('express');
 const app = express();
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 const Users = require('./models/users');
 
 const formidable = require('express-formidable');
